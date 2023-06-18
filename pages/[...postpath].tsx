eb6b548e888681e4c0fde5f92d8ac3c40ab5b0e6
Sanket Dhulap <135593632+sanketdhulap@users.noreply.github.com>
2023-06-18 20:35:44 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint ="https://itechtweets.com/graphql"
+	const endpoint ="https://www.itechtweets.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://itechtweets.com/` + encodeURI(path as string)
+					`https://www.itechtweets.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
